03f98e0d59215463c295fe7bc3584e9a2f4e2d24
watermelon <133223265+watermelonnini@users.noreply.github.com>
2024-07-11 02:53:45 +0800

    Update LoginPageGui.java

--- src/com/apple/view/LoginPageGui.java
+++ src/com/apple/view/LoginPageGui.java
@@ -14,9 +14,13 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JToggleButton;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+
 import java.awt.Color;
 import javax.swing.UIManager;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 
 public class LoginPageGui extends JFrame {
@@ -95,11 +99,28 @@ public class LoginPageGui extends JFrame {
 		passwordField = new JPasswordField();
 		passwordField.setBounds(174, 319, 556, 33);
 		contentPane.add(passwordField);
+	
 		
 		JLabel lblNewLabel_4 = new JLabel("Don't have an account? ");
 		lblNewLabel_4.setFont(new Font("Segoe UI", Font.PLAIN, 16));
 		lblNewLabel_4.setBounds(174, 359, 167, 22);
 		contentPane.add(lblNewLabel_4);
+		
+		JCheckBox showPasswordCheckBox = new JCheckBox("Show Password");
+		showPasswordCheckBox.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		showPasswordCheckBox.setBackground(new Color(192, 192, 192));
+        showPasswordCheckBox.setBounds(736, 319, 120, 33);
+        showPasswordCheckBox.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    passwordField.setEchoChar((char) 0);
+                } else {
+                    passwordField.setEchoChar('\u2022');
+                }
+            }
+        });
+        contentPane.add(showPasswordCheckBox);
+    
 		
 		JToggleButton buttonSignUp = new JToggleButton("Sign up here");
 		buttonSignUp.addActionListener(new ActionListener() {
